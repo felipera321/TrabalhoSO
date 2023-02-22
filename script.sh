d06d192617df2bca13b812ab10e606657f298987
felipera321 <78003467+felipera321@users.noreply.github.com>
2023-02-22 00:19:42 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+
+# Compile the server program
 gcc -o servidorF servidorF.c -lpthread
+
+# Run the server in the background
+./servidorF &
+
+# Wait for the server to start up
+sleep 1
+
+# Compile the client program
 gcc -o client client.c
 
-gnome-terminal -x sh -c "./servidorF; bash"
-for (( i=1; i<=10; i++ ))
-do  
-   echo "Cliente Aberto" $i
-   gnome-terminal -x sh -c "./client; bash"
+# Loop 10 times to run 10 clients
+for i in {1..10}
+do
+    # Start the client in the background
+    ./client &
 done
 
+# Wait for all clients to finish
+wait
+
+# Kill the server process
+pkill servidorF
